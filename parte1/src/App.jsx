44702b235e0da6ca0891ad990eaa86cf7a1e05ba
macalderanqu <mariaalejandra.calderonquevedo@amaris.com>
2024-03-21 04:06:18 -0500

    cambios primera parte 1.1, 1.2, 1.3, 1.5

--- parte1/src/App.jsx
+++ parte1/src/App.jsx
@@ -1,45 +1,47 @@
 
-const App = () => {
+import React from 'react';
+import {Container, Row, Col } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-  const course = {
-    name : 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+const course = {
+  name : 'Half Stack application development',
+  parts: [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+}
 
 
-  const Content = (props) =>{
-    return (
-      <div>
-        <h1>!Welcome to course, {props.name} ¡ </h1>
-        <p>
-          <ul>
-          {props.parts.map((part) => (
-          <><li>{part.name}</li><li>{part.exercises} </li></>
-          ))}
-          </ul>
-        </p>
-      </div>
-    )
-  }
-  
-  return(
+const Content = (props) =>{
+  return (
     <div>
-      <Content name={course.name} parts={course.parts} />
+      <h1>!Welcome to course, {props.name} ¡ </h1>
+      <p>
+        <ul>
+        {props.parts.map((part) => (
+        <><li>{part.name}</li><li>{part.exercises} </li></>
+        ))}
+        </ul>
+      </p>
     </div>
   )
 }
-
+const App = () => {
+  return(
+    <Container>
+      <hr />
+      <Content name={course.name} parts={course.parts} />
+    </Container>
+  )
+}
 export default App
